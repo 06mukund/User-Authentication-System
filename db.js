32b23b95f712b6a48e0484925286c5d7bc128586
Mukund Kumar Ray <mukundkumar0606@gmail.com>
2026-02-28 11:24:11 +0530

    Added auth system project

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Muku@0606",
+    password: " ", // Use your Password
     database: "auth_system"
 });
 
